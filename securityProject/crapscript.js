a6987858d48e7b6a0de43b05e639f1a5a5a28a8a
Ben Morris <benjaminmjmorris@gmail.com>
2017-08-07 18:58:47 -0400

    separateing html from js

---
+++ securityProject/crapscript.js
@@ -0,0 +1,7 @@
+var url = document.URL
+console.log(url)
+var urlsegs = url.split("?")
+var expression = urlsegs[urlsegs.length-1]
+var unexpression = unescape(expression)
+ans = eval(unexpression);
+document.getElementById("answer").innerHTML = ans;
